91999abb64a948dc40c624439f3dd0e1e0867ccf
yousun <a0921990065@gmail.com>
2021-04-15 23:02:43 +0800

    // 加上註解

--- paper_doll/PaperDoll_ViewController.swift
+++ paper_doll/PaperDoll_ViewController.swift
@@ -10,20 +10,21 @@ import AVFoundation
 
 class PaperDoll_ViewController: UIViewController {
     
+    // 左邊多個重疊的 Image 先形成人物，之後透過 Button 轉換圖片
     @IBOutlet weak var HeadImage: UIImageView!
     @IBOutlet weak var FaceImage: UIImageView!
     @IBOutlet weak var BeardImage: UIImageView!
     @IBOutlet weak var GlassesImage: UIImageView!
     @IBOutlet weak var PoseImage: UIImageView!
     
-    
+    // 右邊的框框裡有 5 種不一樣的 ScrollView，透過 Segment 切換來選擇圖片
     @IBOutlet weak var HeadScrollView: UIScrollView!
     @IBOutlet weak var FaceScrollView: UIScrollView!
     @IBOutlet weak var GlassesScrollView: UIScrollView!
     @IBOutlet weak var BeardScrollView: UIScrollView!
     @IBOutlet weak var PoseScrollView: UIScrollView!
     
-    
+    // 抓取打出來的文字，配合 AVSpeechSynthesizer 說話
     @IBOutlet weak var NameTextField: UITextField!
     
     
@@ -33,11 +34,12 @@ class PaperDoll_ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    // 從 TextField 拉 IBAction 修改成 Did End On Exit 用於收鍵盤
     @IBAction func dismissKeyboard(_ sender: Any) {
         view.endEditing(true)
     }
     
-    
+    // if 來判斷 selectedSegmentIndex 選擇的位置 顯示/隱藏 ScrollView
     @IBAction func ChooseSegmented(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0{
             HeadScrollView.isHidden = false
@@ -76,28 +78,25 @@ class PaperDoll_ViewController: UIViewController {
         }
     }
     
+    // ScrollView 裡面的 ImageButton 點選直接跟換
     @IBAction func HeadBtn(_ sender: UIButton) {
         HeadImage.image = sender.currentImage
     }
-    
     @IBAction func FaceBtn(_ sender: UIButton) {
         FaceImage.image = sender.currentImage
     }
-    
     @IBAction func GlassesBtn(_ sender: UIButton) {
         GlassesImage.image = sender.currentImage
     }
-    
     @IBAction func BeardBtn(_ sender: UIButton) {
         BeardImage.image = sender.currentImage
     }
-    
     @IBAction func PoseBtn(_ sender: UIButton) {
         PoseImage.image = sender.currentImage
     }
     
     
-    
+    // 按下 Button 讀出 TextField 裡的文字
     @IBAction func speakBtn(_ sender: Any) {
         
         let speak = AVSpeechUtterance(string: NameTextField.text! + "每天都扶老太太過馬路")
